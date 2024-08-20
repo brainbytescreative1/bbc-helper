5ae2f020175bb6cc5a76272a33a416e49472c282
brainbytescreative1 <taylor.martin+github@brainbytescreative.com>
2024-08-20 11:04:07 -0400

    v1.0.3: global function changes

--- functions/schema.php
+++ functions/schema.php
@@ -2,24 +2,60 @@
 
 if ( class_exists('acf') ) {
 
+    // get schema global function
+    if ( ! function_exists('bbc_get_schema') ) {
+        function bbc_get_schema( $format = false, $json = false, $code = false ) {
+            if ( $format && $json && $code ) {
+
+                // initialize fields
+                $schema_format = null;
+                $upload_schema_json = null;
+                $paste_schema_code = null;
+
+                // determine format
+                $schema_format = $format;
+                if ( $schema_format ) {
+                    if ( $schema_format === 'JSON Upload' ) {
+                        // determine if array
+                        if ( is_array($json) ) {
+                            $upload_schema_json = $json['url'];
+                        }
+
+                        // determine if url
+                        if ( isset( $json['url'] ) ) {
+                            $upload_schema_json = $json['url'];
+                            if ( $upload_schema_json ) {
+                                echo '<script type="application/ld+json" id="page-schema">' . file_get_contents($upload_schema_json) . '</script>';
+                            }
+                        }
+                    } elseif ( $schema_format === 'Paste Code' ) {
+                        // determine if array
+                        if ( !is_array($code) ) {
+                            $paste_schema_code = $code;
+                        }
+                        
+                        // determine if code block
+                        if ( $paste_schema_code ) {
+                            echo '<script type="application/ld+json" id="page-schema">' . $paste_schema_code . '</script>';
+                        }
+                    }
+                }
+
+            } else {
+                // return if all fields not passed
+                return null;
+            }
+        }
+    }
+
     // add page/post schema
     if ( ! function_exists('bbc_add_page_schema_to_header') ) {
         add_action('wp_head', 'bbc_add_page_schema_to_header');
         function bbc_add_page_schema_to_header(){
-            $schema_format = get_field('schema_format');
-            if ( $schema_format ) {
-                if ( $schema_format === 'JSON Upload' ) {
-                    $upload_schema_json = get_field('upload_schema_json');
-                    if ( isset( $upload_schema_json['url'] ) && $upload_schema_json['url'] ) {
-                        echo '<script type="application/ld+json" id="page-schema">' . file_get_contents($upload_schema_json['url']) . '</script>';
-                    }
-                } elseif ( $schema_format === 'Paste Code' ) {
-                    $paste_schema_code = get_field('paste_schema_code');
-                    if ( $paste_schema_code ) {
-                        echo '<script type="application/ld+json" id="page-schema">' . $paste_schema_code . '</script>';
-                    }
-                }
-            }
+
+            // echo global function
+            echo bbc_get_schema( get_field('schema_format'), get_field('upload_schema_json'), get_field('paste_schema_code') );
+            
         }
     }
 
@@ -30,27 +66,10 @@ if ( class_exists('acf') ) {
             $global_schema = get_field('global_schema', 'schema');
             if ( $global_schema ) {
                 foreach ( $global_schema as $schema ) {
-                    $schema_format = null;
-                    if ( isset( $schema['global_schema_module']['schema_format'] ) ) {
-                        $schema_format = $schema['global_schema_module']['schema_format'];
-                    }
-                    if ( $schema_format ) {
-                        if ( $schema_format === 'JSON Upload' ) {
-                            if ( isset ( $schema['global_schema_module']['upload_schema_json'] ) ) {
-                                $upload_schema_json = $schema['global_schema_module']['upload_schema_json'];
-                                if ( isset( $upload_schema_json['url'] ) && $upload_schema_json['url'] ) {
-                                    echo '<script type="application/ld+json" id="global-schema">' . file_get_contents($upload_schema_json['url']) . '</script>';
-                                }
-                            }
-                        } elseif ( $schema_format === 'Paste Code' ) {
-                            if ( isset ( $schema['global_schema_module']['upload_schema_json'] ) ) {
-                                $paste_schema_code = $schema['global_schema_module']['paste_schema_code'];
-                                if ( $paste_schema_code ) {
-                                    echo '<script type="application/ld+json" id="global-schema">' . $paste_schema_code . '</script>';
-                                }
-                            }
-                        }
-                    }
+
+                    // echo global function
+                    echo bbc_get_schema( $schema['global_schema_module']['schema_format'], $schema['global_schema_module']['upload_schema_json'], $schema['global_schema_module']['paste_schema_code'] );
+
                 }
             }
         }
