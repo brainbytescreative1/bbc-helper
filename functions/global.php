8232b517c3eafbff4e57d721f85da5bcb74ec6d2
brainbytescreative1 <113624450+brainbytescreative1@users.noreply.github.com>
2025-02-03 13:08:31 -0500

    add defaults for empty globals

--- functions/global.php
+++ functions/global.php
@@ -16,7 +16,7 @@ function bbc_global_fields( $wp_customize ) {
     'global_email',
     array(
       'label'      => __( 'Global email', 'textdomain' ),
-      'description' => __( '[bbc_global_email link="true"] or set link to false', 'bbc' ),
+      'description' => __( 'Usage: [bbc_global_email link="true"] or set link to false', 'bbc' ),
       'settings'   => 'global_email',
       'priority'   => 101,
       'section'    => 'title_tagline',
@@ -40,7 +40,7 @@ function bbc_global_fields( $wp_customize ) {
     'global_phone',
     array(
       'label'      => __( 'Global Phone', 'textdomain' ),
-      'description' => __( '[bbc_global_phone link="true"] or set link to false', 'bbc' ),
+      'description' => __( 'Usage: [bbc_global_phone link="true"] or set link to false', 'bbc' ),
       'settings'   => 'global_phone',
       'priority'   => 101,
       'section'    => 'title_tagline',
@@ -64,7 +64,7 @@ function bbc_global_fields( $wp_customize ) {
     'global_address',
     array(
       'label'      => __( 'Global address', 'textdomain' ),
-      'description' => __( '[bbc_global_address]', 'bbc' ),
+      'description' => __( 'Usage: [bbc_global_address]', 'bbc' ),
       'settings'   => 'global_address',
       'priority'   => 101,
       'section'    => 'title_tagline',
@@ -80,6 +80,12 @@ add_action( 'customize_register', 'bbc_global_fields' );
 // site name
 if ( ! function_exists('bbc_site_name_shortcode') ) {
   function bbc_site_name_shortcode() {
+    $site_name = get_bloginfo('name');
+    if ( $site_name ) {
+      return $site_name;
+    } else {
+      return '[site_name]';
+    }
     return get_bloginfo('name');
   }
   add_shortcode( 'bbc_site_name', 'bbc_site_name_shortcode' );
@@ -89,7 +95,6 @@ if ( ! function_exists('bbc_site_name_shortcode') ) {
 if ( ! function_exists('bbc_site_url_shortcode') ) {
   function bbc_site_url_shortcode() {
     return '<a href="' . get_site_url() . '">' . get_site_url() . '</a>';
-    return ;
   }
   add_shortcode( 'bbc_site_url', 'bbc_site_url_shortcode' );
 }
@@ -106,16 +111,19 @@ if ( ! function_exists('bbc_global_email_shortcode') ) {
 
     $global_email = get_option('global_email');
 
-    if ( $global_email ) {
+    if ( isset($global_email) && $global_email ) {
       if ( $link['link'] ) {
         return '<a href="mailto:' . $global_email . '">' . $global_email . '</a>';
       } else {
         return $global_email;
       }
+    } else {
+      return 'GLOBAL_EMAIL';
     }
+
   }
-  add_shortcode( 'bbc_global_email', 'bbc_global_email_shortcode');
 }
+add_shortcode( 'bbc_global_email', 'bbc_global_email_shortcode');
 
 // global phone
 if ( ! function_exists('bbc_global_phone_shortcode') ) {
@@ -130,15 +138,16 @@ if ( ! function_exists('bbc_global_phone_shortcode') ) {
     $global_phone = get_option('global_phone');
 
     if ( $global_phone ) {
-
       if ( $link['link'] ) {
         $global_phone_clean = preg_replace('/[^a-z\d]/i', '', $global_phone);
         return '<a href="tel:+1' . $global_phone_clean . '">' . $global_phone . '</a>';
       } else {
         return $global_phone;
       }
-
+    } else {
+      return 'GLOBAL_PHONE';
     }
+
   }
   add_shortcode( 'bbc_global_phone', 'bbc_global_phone_shortcode');
 }
@@ -146,7 +155,12 @@ if ( ! function_exists('bbc_global_phone_shortcode') ) {
 // global address
 if ( ! function_exists('bbc_global_address_shortcode') ) {
   function bbc_global_address_shortcode() {
-    return get_option('global_address');
+    $global_address = get_option('global_address');
+    if ( $global_address ) {
+      return $global_address;
+    } else {
+      return 'GLOBAL_ADDRESS';
+    }
   }
   add_shortcode( 'bbc_global_address', 'bbc_global_address_shortcode' );
 }
